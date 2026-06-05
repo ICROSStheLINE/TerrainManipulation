55edfbacd360839398676ee3ffb31f68cd4290aa
ICROSStheLINE <coolwillrule123@gmail.com>
2026-06-04 21:43:16 -0400

    - Minor change

--- Assets/Scripts/PhysicalProperties.cs
+++ Assets/Scripts/PhysicalProperties.cs
@@ -87,8 +87,6 @@ public class PhysicalProperties : MonoBehaviour
             yield return null;
         }
 
-        emberSmokeObject.transform.SetParent(null);
-        emberSmokeObject.transform.localScale = new Vector3(1,1,1);
         Destroy(gameObject);
     }
 
